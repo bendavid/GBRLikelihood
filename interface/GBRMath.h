2d4632f4ab5fa4f1ee52b53745b8571561c06d77
Josh Bendavid <bendavid@t3btch111.mit.edu>
2013-09-10 07:12:56 -0400

    additional protection for vdt-based pow function

--- interface/GBRMath.h
+++ interface/GBRMath.h
@@ -2,19 +2,23 @@
 #define GBRMATH
 
 #include "vdt/vdtMath.h"
+#include <cmath>
 
 namespace gbrmath {
  
   inline double fast_pow(double base, double exponent) {
     if (base==0. && exponent>0.) return 0.;
     else if (base>0.) return vdt::fast_exp(exponent*vdt::fast_log(base));
-    else return -vdt::fast_exp(exponent*vdt::fast_log(-base));
+    else if (base<0.) return -vdt::fast_exp(exponent*vdt::fast_log(-base));
+    else return std::nan("");
   }
 
   inline float fast_powf(float base, float exponent) {
     if (base==0. && exponent>0.) return 0.;
     else if (base>0.) return vdt::fast_expf(exponent*vdt::fast_logf(base));
-    else return -vdt::fast_expf(exponent*vdt::fast_logf(-base));  }  
+    else if (base<0.) return -vdt::fast_expf(exponent*vdt::fast_logf(-base));
+    else return std::nanf(""); 
+  }
 }
 
 #endif
